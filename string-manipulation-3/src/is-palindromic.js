e87beadd667dc7ce3cf7f4a287734dcbe303ccc9
Thomas Tahk <tomtahk27@gmail.com>
2022-06-17 04:38:41 +0000

    Completed string-manipulation-3 challenge.

---
+++ string-manipulation-3/src/is-palindromic.js
@@ -0,0 +1,11 @@
+/* exported isPalindromic */
+
+function isPalindromic(string) {
+  string = string.replace(/\s+/g, '');
+  for (let i = 0; i < string.length / 2; i++) {
+    if (string[i] !== string[string.length - 1 - i]) {
+      return false;
+    }
+  }
+  return true;
+}
